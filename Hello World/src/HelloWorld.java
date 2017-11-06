972217352e76af0fd5cf9c28a942ace69a0682fd
James <James@192.168.0.15>
2017-11-06 04:39:06 +0000

    Hello

---
+++ Hello World/src/HelloWorld.java
@@ -0,0 +1,8 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) { int n1 = 15; addDouble(n1, 10); System.out.println(n1); }
+	static void addDouble(int n1, int x) { x *= 2; n1 += x; System.out.println(n1); }
+	}
+
+
